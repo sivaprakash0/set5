8fda18b8d63231c600d0a4fef06abd0f05afcced
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-08-15 15:26:24 +0530

    Add files via upload

---
+++ powofposition.java
@@ -0,0 +1,17 @@
+package set5;
+
+import java.util.Scanner;
+
+public class powofposition {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int a=in.nextInt();
+		String s=Integer.toString(a);
+		char c[]=s.toCharArray();
+		int sum=0;
+		for(int i=0;i<s.length();i++){
+			sum=sum+(int)Math.pow(c[i]-48,i);
+		}
+		System.out.println(sum);
+	}
+}
