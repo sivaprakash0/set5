208e9c9dfc058dc7172b49ce8ad958ef585afd12
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-08-15 22:44:42 +0530

    Add files via upload

---
+++ factorialno.java
@@ -0,0 +1,14 @@
+package set6;
+
+public class factorialno {
+	public static void main(String[] args) {
+		int no=Integer.parseInt(args[0]);
+		int sum=1,tot;
+		if(no<=25){
+		for(int i=1;i<no;i++){
+		tot=i+1;
+		sum=sum*tot;
+		}
+		System.out.println(sum);}
+	}
+}
