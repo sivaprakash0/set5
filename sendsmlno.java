6985c4e83a132e431aa88d06443ff307e38c4cc3
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-08-15 22:18:16 +0530

    Add files via upload

---
+++ sendsmlno.java
@@ -0,0 +1,23 @@
+package set6;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class sendsmlno {
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	int sz=in.nextInt();
+	int ar[]=new int[sz];
+	for(int i=0;i<sz;i++){
+		ar[i]=in.nextInt();
+	}
+	Arrays.sort(ar);
+	for(int j=0;j<sz;j++){
+		if(ar[j]<ar[j+1]){
+			System.out.println(ar[j+1]);
+			break;
+		}
+	}
+	
+}
+}
