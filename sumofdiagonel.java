02d4591e11b92aef1c1d17db68b9534074acdd5d
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-08-15 15:21:04 +0530

    Add files via upload

---
+++ sumofdiagonel.java
@@ -0,0 +1,22 @@
+package set5;
+
+import java.util.Scanner;
+public class sumofdiagonel {
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	int a=in.nextInt();
+	if(a<=1000){
+	int ar[][]=new int[a][a];
+	for(int i=0;i<a;i++){
+		for(int j=0;j<a;j++){
+		ar[i][j]=in.nextInt();
+	 }
+	}	
+	int sum=0;
+	for(int i=0;i<a;i++){
+		sum=ar[i][i]+sum;
+	}
+	System.out.println(sum);
+	}
+}
+}
