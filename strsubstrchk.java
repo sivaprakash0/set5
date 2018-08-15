3e613cf88f0e9761b04bd2f43e32a007cc310f89
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-08-15 15:41:30 +0530

    Add files via upload

---
+++ strsubstrchk.java
@@ -0,0 +1,27 @@
+package set5;
+
+import java.util.Scanner;
+
+public class strsubstrchk {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		String s=in.next();
+		String s1=in.next();
+		int l=s1.length();
+		int c=0,cnt=0;
+		for(int i=0;i<s.length()-l;i++){
+			String a=s.substring(i,i+l);
+			if(a.equals(s1)){
+				c=i;
+				cnt++;
+				break;
+			}
+		}
+		if(cnt==0){
+			System.out.println("no");
+		}
+		else{
+			System.out.println("yes");
+		}
+	}
+}
