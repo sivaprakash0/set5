468d337986acf70d4b97a4c3a884b2a119e512cb
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-08-15 15:31:23 +0530

    Add files via upload

---
+++ sumofasciivalue.java
@@ -0,0 +1,16 @@
+package set5;
+
+import java.util.Scanner;
+
+public class sumofasciivalue {
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	String s=in.next();
+	char c[]=s.toCharArray();
+	int sum=0;
+	for(int i=0;i<s.length();i++){
+		sum=sum+c[i];
+    }
+	System.out.println(sum);
+}
+}
