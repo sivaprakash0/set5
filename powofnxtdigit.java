e1aa8725853d02fbd2621fb77cd311a20fd9164b
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-08-15 15:11:26 +0530

    Add files via upload

---
+++ powofnxtdigit.java
@@ -0,0 +1,23 @@
+package set5;
+
+import java.util.Scanner;
+public class powofnxtdigit {
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	int a=in.nextInt();
+	String s=Integer.toString(a);
+	char c[]=s.toCharArray();
+	int sum=0,tot;
+	if(s.length()==1){
+		tot=(int)Math.pow(c[0]-48, 2);
+		System.out.println(tot);
+	}
+	else{
+	for(int i=0;i<s.length()-1;i++){
+		sum=sum+(int)Math.pow(c[i]-48, c[i+1]-48);
+	}
+	tot=(int)Math.pow(c[s.length()-1]-48, c[0]-48);
+	System.out.println(sum+tot);
+   }
+}
+}
